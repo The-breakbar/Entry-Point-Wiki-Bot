de4cd871a5ef0d85197bdd86cc7c9b4f2df67828
break-bar <sonicdonald@gmail.com>
2021-08-07 12:11:46 +0200

    replaced dotenv package with vscode .env

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { Client, Intents } = require("discord.js");
 const fs = require("fs");
-require("dotenv").config();
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
