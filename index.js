18cd158c935ea489278daa57a9b6ad91ffcbe577
break-bar <sonicdonald@gmail.com>
2021-08-07 12:03:31 +0200

    basic bot setup and login, dotenv, loading of files for event handlers

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log(`Hello world`);
+const { Client, Intents } = require("discord.js");
+const fs = require("fs");
+require("dotenv").config();
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+const eventFiles = fs.readdirSync("./events").filter((file) => file.endsWith(".js"));
+
+eventFiles.forEach((file) => {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+});
+
+client.login(process.env.TOKEN);
