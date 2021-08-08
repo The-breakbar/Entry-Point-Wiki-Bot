825071b4a6ce2cafcadf4cb2b36ed776c0aabf1b
The-breakbar <breakbar.email@gmail.com>
2021-08-08 12:53:03 +0200

    new files for commands

---
+++ commands/squadPing.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: "squadping",
+	description: "Ping for a squad if you need players or request a host for an expansion mission.",
+	async execute(interaction, client) {
+		// Create modal
+		// Create event listeners
+		// Finish ping process
+	}
+};
