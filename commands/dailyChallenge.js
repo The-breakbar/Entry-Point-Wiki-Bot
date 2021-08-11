4c009710fbcdd7864badc434de803b1b7df6420e
The-breakbar <breakbar.email@gmail.com>
2021-08-11 21:44:45 +0200

    added remaining time to dailyChallenge command

--- commands/dailyChallenge.js
+++ commands/dailyChallenge.js
@@ -25,10 +25,40 @@ module.exports = {
 		const mod2 = wikitext.split("33%")[2].split("|")[1].trim();
 		const mod3 = wikitext.split("33%")[2].split("|")[2].trim();
 
+		// Get remaining challenge time
+		const now = new Date();
+		// Check daylight saving time in EST (correct until 2026)
+		let DST = false;
+		const year = now.getUTCFullYear();
+		const dstStart = new Date(`March 1, ${year} 02:00:00 EST`);
+		const dstEnd = new Date(`November 1, ${year} 02:00:00 EST`);
+		dstStart.setDate(dstStart.getDate() + (14 - dstStart.getDay()));
+		dstEnd.setDate(dstEnd.getDate() + (7 - dstEnd.getDay()));
+		if (now >= dstStart && now <= dstEnd) {
+			DST = true;
+		}
+		// Get remaining time until EST midnight
+		const wantedTime = new Date(now);
+		wantedTime.setUTCHours(DST ? 4 : 5, 0, 0, 0);
+		if (wantedTime < now) {
+			wantedTime.setDate(wantedTime.getDate() + 1);
+		}
+		let delta = Math.round((wantedTime - now) / 1000);
+		const hours = Math.floor(delta / 3600) % 24;
+		delta -= hours * 3600;
+		const minutes = Math.floor(delta / 60) % 60;
+		delta -= minutes * 60;
+		const seconds = delta;
+
 		// Create embed
 		const embed = new MessageEmbed()
 			.setTitle(title)
 			.addFields({ name: modTitle1, value: mod1 }, { name: modTitle2, value: mod2 }, { name: modTitle3, value: mod3 })
+			.setFooter(
+				`${hours} hour${hours != 1 ? "s" : ""} and ${minutes} minute${
+					minutes != 1 ? "s" : ""
+				} left until next challenge.`
+			)
 			.setColor(global.purple);
 
 		// Reply
