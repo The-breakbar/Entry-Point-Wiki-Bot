e3af46cdb4a888a333134718984dba31aa445c8b
The-breakbar <breakbar.email@gmail.com>
2021-08-27 21:51:17 +0200

    added guildMemberAdd event log

---
+++ events/log-events/guildMemberAdd.js
@@ -0,0 +1,28 @@
+module.exports = {
+	name: "guildMemberAdd",
+	async execute(member, client) {
+		// Only log for wiki server
+		if (message.guild != client.wikiServer.guild) return;
+
+		const embed = {
+			color: "GREEN",
+			author: {
+				name: "Member joined",
+				icon_url: member.user.displayAvatarURL()
+			},
+			description: `${member.user} ${member.user.tag}`,
+			fields: [
+				{
+					name: "Account creation",
+					value: `<t:${member.user.createdTimestamp}:R>`
+				}
+			],
+			timestamp: new Date(),
+			footer: {
+				text: `ID: ${member.user.id}`
+			}
+		};
+
+		await client.wikiServer.log.send({ embeds: [embed] });
+	}
+};
