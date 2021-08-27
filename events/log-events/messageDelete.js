690b800f32d0b470722433786c200422452d6eb2
The-breakbar <breakbar.email@gmail.com>
2021-08-27 21:51:39 +0200

    fixed message delete log

--- events/log-events/messageDelete.js
+++ events/log-events/messageDelete.js
@@ -1,6 +1,9 @@
 module.exports = {
 	name: "messageDelete",
 	async execute(message, client) {
+		// Only log for wiki server
+		if (message.guild != client.wikiServer.guild) return;
+
 		let { author, channel, content, attachments } = message;
 
 		const embed = {
@@ -11,7 +14,7 @@ module.exports = {
 			},
 			description: `
 				**Message sent by ${author} deleted in ${channel}**${content != "" ? "\n" + content : ""}${
-				attachments.size > 0 ? "\n" + `[Attachment (.${attachments.first().url.split(".").slice(-1)})](${attachments.first().url})` : ""
+				attachments.size > 0 ? "\n" + `[Attachment (.${attachments.first().url.split("/").slice(-1)[0].split(".").slice(-1)})](${attachments.first().url})` : ""
 			}`,
 			timestamp: new Date()
 		};
