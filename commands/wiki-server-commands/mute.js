6c1dc19e75d821c94e987b83ce93db68900dacbf
The-breakbar <breakbar.email@gmail.com>
2021-08-27 23:50:25 +0200

    added mute command

---
+++ commands/wiki-server-commands/mute.js
@@ -0,0 +1,56 @@
+const { mute } = require("../../utils/muteUtils");
+
+module.exports = {
+	name: "mute",
+	description: "Mute command for server staff",
+	options: [
+		{
+			name: "user",
+			type: "USER",
+			description: "A user ping, or their user id",
+			required: true
+		},
+		{
+			name: "length",
+			type: "STRING",
+			description: "A number and a letter (m = minute, h = hour, d = day) e.g. 15m / 6h / 1d",
+			required: true
+		}
+	],
+	permissions: ["621679404636176384", "718954666032889866", "621679757221822465"], // Wiki admin, Server mod, Wiki mod
+	async execute(interaction, client) {
+		const member = interaction.options.getMember("user");
+		const muteLength = interaction.options.getString("length");
+
+		// Check if mute length is valid
+		if (/^([1-9]|[1-9][0-9])[dhm]$/.test(muteLength.trim())) {
+			// Parse mute length
+			const type = muteLength.slice(-1);
+			const timeNumber = muteLength.slice(0, muteLength.length == 2 ? 1 : 2);
+
+			let time;
+			switch (type) {
+				case "m":
+					time = timeNumber * 1000 * 60;
+					break;
+				case "h":
+					time = timeNumber * 1000 * 60 * 60;
+					break;
+				case "d":
+					time = timeNumber * 1000 * 60 * 60 * 24;
+					break;
+			}
+
+			const typeText = type == "m" ? "minute" : type == "h" ? "hour" : "day";
+			const embed = {
+				description: `Muted ${member.user} for ${timeNumber} ${typeText}${timeNumber == 1 ? "" : "s"}`,
+				color: global.purple
+			};
+
+			await mute(member, time);
+			await interaction.reply({ embeds: [embed] });
+		} else {
+			await interaction.reply({ content: "Invalid mute length, see description for proper usage (e.g. 15m / 6h / 1d).", ephemeral: true });
+		}
+	}
+};
---
+++ utils/muteUtils.js
@@ -0,0 +1,72 @@
+const redis = require("redis");
+const { promisify } = require("util");
+
+// Connect to redis database which holds mute durations
+const redisClient = redis.createClient({
+	host: process.env.REDIS_HOST,
+	port: process.env.REDIS_PORT,
+	password: process.env.REDIS_PASSWORD
+});
+
+// Client events
+redisClient.on("error", (error) => {
+	console.error(error);
+});
+
+redisClient.on("ready", () => {
+	redisClient.keys("*", (error, result) => {
+		console.log(`Connected to Redis database, found ${result.length} active mutes`);
+		// TEMP
+		result.forEach((key) => {
+			redisClient.del(key);
+		});
+	});
+});
+
+// Promisify redis commands
+const set = promisify(redisClient.set).bind(redisClient);
+const del = promisify(redisClient.del).bind(redisClient);
+const keys = promisify(redisClient.keys).bind(redisClient);
+const get = promisify(redisClient.get).bind(redisClient);
+const hmget = promisify(redisClient.hmget).bind(redisClient);
+
+module.exports = {
+	async mute(member, time) {
+		let endTime = Date.now() + time;
+
+		// Add mute in database
+		await set(member.user.id, endTime);
+
+		// Add muted role
+		let mutedMember = member;
+		if (!mutedMember.roles.cache.some((role) => role.name == "Muted")) {
+			mutedMember = await member.roles.add(member.guild.roles.cache.find((role) => role.name == "Muted"));
+		}
+
+		// Set unmute timeout
+		setTimeout(() => {
+			get(mutedMember.user.id).then((resultTime) => {
+				// Only unmute if mute hasn't been updated to a longer time
+				if (resultTime <= endTime) {
+					// Remove from database
+					del(mutedMember.user.id);
+
+					// Unmute
+					if (mutedMember.roles.cache.some((role) => role.name == "Muted")) {
+						mutedMember.roles.remove(mutedMember.guild.roles.cache.find((role) => role.name == "Muted"));
+					}
+				}
+			});
+		}, time);
+	},
+	// Get list of currently muted users
+	async getMuted() {
+		let dbKeys = await keys("*");
+
+		let muteTimes;
+		if (dbKeys.length > 1) muteTimes = await hmget(dbKeys);
+		else if (dbKeys.length == 1) muteTimes = [await get(dbKeys[0])];
+
+		return dbKeys.map((dbKey, index) => [dbKey, muteTimes[index]]);
+	}
+};
