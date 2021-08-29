cb48795fc750da462ffe8f801b75617d5f66099f
The-breakbar <breakbar.email@gmail.com>
2021-08-29 22:07:52 +0200

    added purge command

---
+++ commands/wiki-server-commands/purge.js
@@ -0,0 +1,23 @@
+module.exports = {
+	name: "purge",
+	description: "Purge command for server staff",
+	options: [
+		{
+			name: "count",
+			type: "INTEGER",
+			description: "Amount of messages to purge",
+			required: true
+		}
+	],
+	permissions: ["621679404636176384", "718954666032889866", "621679757221822465"],
+	async execute(interaction, client) {
+		await interaction.deferReply({ ephemeral: true });
+
+		// Delete messages in channel
+		let count = interaction.options.getInteger("count");
+		count = count > 100 ? 100 : count;
+		if (count > 0) await interaction.channel.bulkDelete(count);
+
+		await interaction.editReply({ content: `Purged ${count} message${count == 1 ? "" : "s"}.` });
+	}
+};
