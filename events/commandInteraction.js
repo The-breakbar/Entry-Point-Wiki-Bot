26e6e216d2077874e8d6369ffed2627a53720802
The-breakbar <breakbar.email@gmail.com>
2021-08-29 21:36:58 +0200

    added additional error catching for commands

--- events/commandInteraction.js
+++ events/commandInteraction.js
@@ -5,6 +5,7 @@ module.exports = {
 		if (!interaction.isCommand()) return;
 		if (!client.commands.has(interaction.commandName)) return;
 
+		// Catch command execution error
 		try {
 			// Execute command
 			const command = client.commands.get(interaction.commandName);
@@ -17,20 +18,26 @@ module.exports = {
 				await command.execute(interaction, client);
 			}
 		} catch (error) {
-			// Notify user on error
 			console.log(error);
-			const errorMessage = {
-				content: `There was an error while executing the \`${interaction.commandName}\` command! Please send this message or a screenshot of it to <@${client.wikiServer.guild.ownerId}>.\n\`\`\`${error.stack}\`\`\``,
-				embeds: [],
-				components: [],
-				ephemeral: true
-			};
 
-			// Check if interaction has been deferred or replied to
-			if (interaction.deferred || interaction.replied) {
-				await interaction.followUp(errorMessage);
-			} else {
-				await interaction.reply(errorMessage);
+			// Catch incase interaction isn't valid anymore
+			try {
+				// Notify user on error
+				const errorMessage = {
+					content: `There was an error while executing the \`${interaction.commandName}\` command! Please send this message or a screenshot of it to <@${client.wikiServer.guild.ownerId}>.\n\`\`\`${error.stack}\`\`\``,
+					embeds: [],
+					components: [],
+					ephemeral: true
+				};
+
+				// Check if interaction has been deferred or replied to
+				if (interaction.deferred || interaction.replied) {
+					await interaction.followUp(errorMessage);
+				} else {
+					await interaction.reply(errorMessage);
+				}
+			} catch (error) {
+				console.error(error);
 			}
 		}
 	}
