c94a839110937c4b8bcb52517889487216448dc6
The-breakbar <breakbar.email@gmail.com>
2021-09-02 16:16:54 +0200

    added ban logging

---
+++ events/log/guildBanAdd.js
@@ -0,0 +1,27 @@
+module.exports = {
+	name: "guildBanAdd",
+	async execute(ban, client) {
+		if (ban.guild != client.wikiServer.guild) return;
+
+		const embed = {
+			color: "RED",
+			author: {
+				name: "Member banned",
+				icon_url: ban.user.displayAvatarURL()
+			},
+			description: ban.user,
+			fields: [
+				{
+					name: "Reason",
+					value: ban.reason ? ban.reason : "No reason specified"
+				}
+			],
+			timestamp: new Date(),
+			footer: {
+				text: `ID: ${ban.user.id}`
+			}
+		};
+
+		await client.wikiServer.log.send({ embeds: [embed] }).catch((error) => console.error(error));
+	}
+};
