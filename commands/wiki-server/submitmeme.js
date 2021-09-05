cf754698e2db809556e677e2eb2e33d5f4c49ca0
The-breakbar <breakbar.email@gmail.com>
2021-09-05 16:56:54 +0200

    fixed url regex for submitmeme

--- commands/wiki-server/submitmeme.js
+++ commands/wiki-server/submitmeme.js
@@ -6,11 +6,13 @@ module.exports = {
 		await interaction.deferReply({ ephemeral: true });
 
 		const url = interaction.options.getString("url");
-		if (/https?:\/\/cdn\.discordapp\.com\/attachments\//.test(url)) {
+		if (/https?:\/\//.test(url)) {
 			await client.wikiServer.submitMeme.send({ content: url }).catch((error) => console.error(error));
 			await interaction.editReply({ content: "Thank you for your submission!" });
 		} else {
-			await interaction.editReply({ content: `Please submit the meme in the form of a discord url (send the meme in any channel, right-click and "Copy link").` });
+			await interaction.editReply({
+				content: `Please submit the meme in the form of a discord url (send the meme in any channel, right-click on the image and "Copy link").`
+			});
 		}
 	}
 };
