9a2503f84a3a02a8665ccd2da58f62be8429f31a
BreezeQS <70638053+BreezeQS@users.noreply.github.com>
2021-10-03 14:05:12 +0300

    Update links.js

--- commands/links.js
+++ commands/links.js
@@ -19,12 +19,11 @@ const linkEmbed = {
 	[Entry Point Merch](https://www.amazon.com/stores/page/C2FE1530-D7DA-4BD6-86C5-00DC5F582A39)
 	[Freefall Softworks Group](https://www.roblox.com/groups/4061796/Freefall-Softworks)
 	[Speedrun Leaderboards](https://www.speedrun.com/rblx_entrypoint/levels)
-	[Update Log Archive](https://discordapp.com/invite/ZTH424b)
 	[Entry Point Wiki Discord](https://discord.gg/wacqqFb)
 	[EEPC Discord](https://discord.com/invite/xNyXSPV)
 	[Halcyon and Phoenix Safehouse](https://www.roblox.com/games/6523072186/Halcyon-and-Phoenix-Safehouse)
 	[Halcyon and Phoenix Picnic](https://www.roblox.com/games/4701103583/Halcyon-and-Phoenix-Enemies-for-Life-Picnic-2020)
 	[r/entrypoint](https://www.reddit.com/r/entrypoint/)
-	[r/freefallsoftworks](https://www.reddit.com/r/entrypoint/)
+	[r/freefallsoftworks](https://www.reddit.com/r/FreefallSoftworks/)
 	`
 };
