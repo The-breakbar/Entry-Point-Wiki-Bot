ab34bb9083d1be111ebedfdb957d37a22c345dfe
The-breakbar <breakbar.email@gmail.com>
2021-10-04 23:54:33 +0200

    improved error handling of search command

--- commands/search.js
+++ commands/search.js
@@ -13,48 +13,49 @@ module.exports = {
 		// Make API call to wiki
 		const query = interaction.options.getString("query");
 		const queryDisplay = query.length > 50 ? query.slice(0, 50) + "..." : query;
-		await fetch(`https://entry-point.fandom.com/api.php?action=opensearch&format=json&search=${query}&namespace=0&limit=5&redirects=resolve`)
-			.then((fetchReponse) => {
-				const response = fetchReponse.json();
-				// Check for results
-				if (response[1].length == 0) {
-					const failEmbed = {
-						color: global.purple,
-						description: `There were no search results for "${queryDisplay}"`
-					};
-					interaction.editReply({ embeds: [failEmbed] });
-				} else {
-					// Parse response
-					const links = response[3];
-					const linkTitels = response[1];
-
-					const row = new MessageActionRow({
-						components: links.map((link, index) => {
-							return new MessageButton({
-								style: "LINK",
-								label: linkTitels[index],
-								url: link
-							});
-						})
-					});
-
-					const embed = {
-						color: global.purple,
-						description: `Search results for "${queryDisplay}"`
-					};
-
-					// Reply with links
-					interaction.editReply({ embeds: [embed], components: [row] });
-				}
-			})
-			.catch((error) => {
-				// If fetch fails
+		const response = await fetch(`https://entry-point.fandom.com/api.php?action=opensearch&format=json&search=${query}&namespace=0&limit=5&redirects=resolve`).catch(
+			(error) => {
 				console.error(error);
-				const errorEmbed = {
+			}
+		);
+
+		if (response.ok) {
+			const data = await response.json();
+			if (data[1].length == 0) {
+				const failEmbed = {
 					color: global.purple,
-					description: "There was an error while searching, please try again."
+					description: `There were no search results for "${queryDisplay}"`
 				};
-				interaction.editReply({ embeds: [errorEmbed] });
-			});
+				interaction.editReply({ embeds: [failEmbed] });
+			} else {
+				// Parse response
+				const links = data[3];
+				const linkTitels = data[1];
+
+				const row = new MessageActionRow({
+					components: links.map((link, index) => {
+						return new MessageButton({
+							style: "LINK",
+							label: linkTitels[index],
+							url: link
+						});
+					})
+				});
+
+				const embed = {
+					color: global.purple,
+					description: `Search results for "${queryDisplay}"`
+				};
+
+				// Reply with links
+				interaction.editReply({ embeds: [embed], components: [row] });
+			}
+		} else {
+			const errorEmbed = {
+				color: global.purple,
+				description: "There was an error while searching, please try again."
+			};
+			interaction.editReply({ embeds: [errorEmbed] });
+		}
 	}
 };
